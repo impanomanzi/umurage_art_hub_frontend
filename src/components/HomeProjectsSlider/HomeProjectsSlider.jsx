bd970cde1d84e3020d158f0d70424132d77770fa
impanomanzi <impanomanzienock@gmail.com>
2023-12-29 11:45:24 +0200

    home project card

--- src/components/HomeProjectsSlider/HomeProjectsSlider.jsx
+++ src/components/HomeProjectsSlider/HomeProjectsSlider.jsx
@@ -3,36 +3,39 @@ import Buttton from "react-bootstrap/Button";
 import { Carousel, Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "../ProjectCard/ProjectCard";
 import "./HomeProjectsSlider.css";
+import "bootstrap/dist/css/bootstrap.css";
 import { Link } from "react-router-dom";
 import settings from "../settings.json";
 
 function HomeProjectsSlider(props) {
   return (
-    <Container className="card carousel-container">
-      <div className="carousel-left"></div>
-      <Carousel variant="dark" className="carousel" slide={false} fade>
-        {props.projects.map((project, index) => {
-          return (
-            <Carousel.Item interval={1500}>
-              <img
-                src={project.image.replace(
-                  "http://localhost:5000",
-                  `${settings.server_domain}`
-                )}
-              />
-              {/* <Carousel.Caption>
+    <div className="card">
+      <Container className="card carousel-container">
+        <div className="carousel-left"></div>
+        <Carousel variant="dark" className="carousel" slide={false} fade>
+          {props.projects.map((project, index) => {
+            return (
+              <Carousel.Item interval={1500}>
+                <img
+                  src={project.image.replace(
+                    "http://localhost:5000",
+                    `${settings.server_domain}`
+                  )}
+                />
+                {/* <Carousel.Caption>
                 <div className="slider-meta-container">
                   <Link to={`/payment/${project.id}`}>
                     <button>Join</button>
                   </Link>
                 </div>
               </Carousel.Caption> */}
-            </Carousel.Item>
-          );
-        })}
-      </Carousel>
-      <div className="carousel-right"></div>
-    </Container>
+              </Carousel.Item>
+            );
+          })}
+        </Carousel>
+        <div className="carousel-right"></div>
+      </Container>
+    </div>
   );
 }
 

6e6ef7072422883f3a158a0c9c60d087f098a0d6
IMPANO <impanomanzienock@gmail.com>
2024-07-28 17:55:24 +0200

    increasing image quality

--- src/components/HomeProjectsSlider/HomeProjectsSlider.jsx
+++ src/components/HomeProjectsSlider/HomeProjectsSlider.jsx
@@ -10,11 +10,18 @@ function HomeProjectsSlider(props) {
       <div className="carousel-left"></div>
       <Carousel className="carousel" slide={false} fade>
         {props.projects.map((project, _) => {
+          // let imageUrl = project.image;
+          // let index1 = imageUrl.indexOf("upload/") + "upload/".length;
+          // let newUrl =
+          //   imageUrl.substring(0, index1) +
+          //   "c_scale,o_100,h_200,w_200/" +
+          //   imageUrl.substring(index1, imageUrl.length);
+
           let imageUrl = project.image;
           let index1 = imageUrl.indexOf("upload/") + "upload/".length;
           let newUrl =
             imageUrl.substring(0, index1) +
-            "c_scale,o_100,h_200,w_200/" +
+            "q_auto:best/" +
             imageUrl.substring(index1, imageUrl.length);
           return (
             <Carousel.Item interval={1500} a>

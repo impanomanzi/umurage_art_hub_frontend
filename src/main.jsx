a7f20ac5ec88662eb28a91340c98148f40e93ac8
IMPANO <impanomanzienock@gmail.com>
2024-08-07 17:05:29 +0200

    solving problem

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import ThreeGallery from "./components/ThreeGallery/ThreeGallery.jsx";
 import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <ThreeGallery />
+    <App />
   </React.StrictMode>
 );
